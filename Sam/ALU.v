c62b354e354ce2ce69ffdabe5f87e92207e1f760
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2024-09-09 15:32:00 -0400

    Skeleton

--- Sam/ALU.v
+++ Sam/ALU.v
@@ -2,20 +2,68 @@
 // This file is where you'll define your simple ALU for project 1.
 // Refer to the project one document for instructions and guidance.
 
-module ALU (Insert Parameters here);
+module ALU ();
 
     // Input definitions here
-    input CLK, RST, Enable
-    input reg [15:0] Data_A;         // 16-bits of data for A
-    input reg [15:0] Data_B;         // 16-bits of data for B
-    input reg [3:0] Opcaode;
+    input CLK, RST, Enable;
+    reg [15:0] Data_A, Data_B;         // 16-bits of data for A
+    reg [3:0] Opcaode;
 
     // Output defintions here
-    output CF                       // overflow bit 
+    output CF;                  // overflow bit 
     output reg [15:0] Results;      // 16-bits 
 
+    // Define States for ALU
+    parameter  S_000 = 0, S_001 = 1, S_010 = 2, S_011 = 3, S_100 = 4, S_101 = 5, S_110 = 6, S_111 = 7;
 
-// Implementation here
+    // Variables for Sequence Logic
+    reg [3:0] State, StateNext;
+
+    always @(State) begin
+      case (State)
+	    S_000: begin
+            StateNext <= S_001;
+	    end
+	    
+        S_001: begin
+	        StateNext <= S_010;
+	    end
+
+	    S_010: begin
+	        StateNext <= S_011;
+	    end
+
+	    S_011: begin
+	        StateNext <= S_100;
+	    end
+
+	    S_100: begin
+	        StateNext <= S_101;
+	    end
+
+	    S_101: begin
+	        StateNext <= S_110;
+	    end
+
+	    S_110: begin
+	        StateNext <= S_111;
+	    end
+
+	    S_111: begin
+	        StateNext <= S_000;
+	   end
+
+      endcase // case (State)
+   end // always @ (State)
+
+    //StateReg
+    always @(posedge CLK) begin
+        if (RST == 1) begin
+	        State <= S_000;
+        end else begin
+	        State <= StateNext;
+        end
+    end
 
 // Bunch of condiional statements depending on what the opcode is
 // If the opcode starts with a 0, then take input
