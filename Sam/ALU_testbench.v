8494bcb97d9f900e04c353db07cecc52c6c75853
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2024-09-09 16:44:32 -0400

    Initial Testbench Formation

--- Sam/ALU_testbench.v
+++ Sam/ALU_testbench.v
@@ -2,10 +2,43 @@
 // for project 1. Refer to the project one document for instructions 
 // and guidance.
 
-`timescale 10ms/1ms
+`timescale 1ms/1ms
 
 module ALU_testbench();
 
-// Instantiate your ALU and implement your testbench here
+    reg CLK_s, RST_s, Enable_s;
+    reg [15:0] Data_A_s; // these had to be regs for some reason
+    reg [15:0] Data_B_s; 
+    reg [3:0] Opcode_s;
 
-endmodule
+    // Output defintions here
+    reg CF;                   
+    output wire [15:0] Results;      
+
+    ALU ALU_Test_Functions(CLK_s, RST_s, Enable_s, Data_A_s, Data_B_s, Opcode_s);
+
+    // Basic Clock
+    always begin
+        CLK_s <= 0;
+        #10;
+        CLK_s <= 1;
+        #10;
+    end
+
+    initial begin
+        $dumpvars(0,ALU_testbench); // Include name of tb file
+
+        RST_s <= 1;
+        #10;
+        @(posedge CLK_s)
+        RST_s <= 0;
+        #20;
+        @(posedge CLK_s)
+        Data_A_s = 6464;
+        Data_B_s = 4646;
+        Opcode_s = 001;
+        #20;
+
+    end // Initial Begin
+
+endmodule // End of ALU Testbench
