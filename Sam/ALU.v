68a8abc9675e0f8d33cdf791b949ca9fb23d6b74
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2024-09-09 16:44:04 -0400

    State Before Debugging Initial Testbench Formation

--- Sam/ALU.v
+++ Sam/ALU.v
@@ -2,33 +2,41 @@
 // This file is where you'll define your simple ALU for project 1.
 // Refer to the project one document for instructions and guidance.
 
-module ALU ();
+`timescale 1ms/1ms
+
+module ALU (CLK, RST, Enable, Data_A, Data_B, Opcode);
 
     // Input definitions here
     input CLK, RST, Enable;
-    reg [15:0] Data_A, Data_B;         // 16-bits of data for A
-    reg [3:0] Opcaode;
+    input [15:0] Data_A;
+    input [15:0] Data_B;         // 16-bits of data for A
+    input [3:0] Opcode;
 
     // Output defintions here
     output CF;                  // overflow bit 
     output reg [15:0] Results;      // 16-bits 
 
     // Define States for ALU
-    parameter  S_000 = 0, S_001 = 1, S_010 = 2, S_011 = 3, S_100 = 4, S_101 = 5, S_110 = 6, S_111 = 7;
+    parameter  S_000 = 000, S_001 = 001, S_010 = 010, S_011 = 011, S_100 = 100, S_101 = 101, S_110 = 110, S_111 = 111;
 
     // Variables for Sequence Logic
     reg [3:0] State, StateNext;
 
     always @(State) begin
+
       case (State)
+
 	    S_000: begin
             StateNext <= S_001;
+            #10; // Simply Does Nothing
 	    end
 	    
         S_001: begin
-	        StateNext <= S_010;
+            Results = Data_A + Data_B;
+            #10;
+	        StateNext <= S_000;
 	    end
-
+/*
 	    S_010: begin
 	        StateNext <= S_011;
 	    end
@@ -52,7 +60,7 @@ module ALU ();
 	    S_111: begin
 	        StateNext <= S_000;
 	   end
-
+*/
       endcase // case (State)
    end // always @ (State)
 
