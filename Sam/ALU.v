cac3836a6e53bd9ea54384160bf4afbf8d3ca473
vinny05204 <vga05204@uga.edu>
2024-09-11 18:28:03 -0400

    saving work

--- Sam/ALU.v
+++ Sam/ALU.v
@@ -4,120 +4,154 @@
 
 `timescale 1ns/1ns
 
-module ALU (CLK, RST, Enable);
+module ALU (CLK, RST, Enable, X, Y, Opcode, CF, Results);
 
-    // Input definitions here
-    input CLK, RST, Enable;
-    reg [15:0] Data_A;
-    reg [15:0] Data_B;
-    reg [3:0] Opcode;
+   // Input definitions here
+   input CLK, RST, Enable;
+   input [15:0]	X, Y;
+   input [2:0] Opcode;
+   
+   reg [15:0] Data_A;
+   reg [15:0] Data_B;
+   reg [16:0] OverflowCheck;
+   
 
-    // Output defintions here
-    output reg CF;                  // overflow bit 
-    output reg [15:0] Results;
+   
+   // Output defintions here
+   output reg	     CF;                  // overflow bit 
+   output reg [15:0] Results;
 
-    // Define States for ALU
-    parameter  S_000 = 000, S_001 = 001, S_010 = 010, S_011 = 011, S_100 = 100, S_101 = 101, S_110 = 110, S_111 = 111;
+   // Define States for ALU
+   parameter	     S_000 = 'b000, S_001 = 'b001, S_010 = 'b010, S_011 = 'b011, S_100 = 'b100, S_101 = 'b101, S_110 = 'b110, S_111 = 'b111;
+   
+   // Variables for Sequence Logic
+   reg [3:0]	     State, StateNext;
 
-    // Variables for Sequence Logic
-    reg [3:0] State, StateNext;
+   // At Reset = 1, all internal registers should be in a known state or set to 0. 
+   always @(posedge CLK) begin
+      if (RST == 1) begin
+	 Data_A = 0;
+	 Data_B = 0;
+	 
+         // Reset everything to Zero
+         Results = 0;
+	 State = S_000;
+	 StateNext = S_000;
+	 CF = 0;
+	 OverflowCheck = 0;
+	 
+      end else begin
+	 
+	 // put this if statement around all of the loop so as to enable and disable the loop
+	 if (Enable == 1) begin
 
-    always @(State) begin
+            case (Opcode)
+	      
+              S_000: begin
+		 // NOP Operation
+		 // Set Data A and Data B to X and Y, respectively
+		 Data_A <= X;
+		 Data_B <= Y;
+		 // Simply Does Nothing
+              end // S_000
+              
+              S_001: begin
+		 // A + B
+		 Data_A = X;
+		 Data_B = Y;
+		 
+		 Results = Data_A + Data_B;
+		 		 
+		 OverflowCheck = Data_A + Data_B;
+		 // checks for overflow
+		 if (OverflowCheck[16] == 1) begin
+		    CF <= 1;
+		 end else begin
+		    CF <= 0;
+		 end
+		 
+              end // S_001
 
-        // put this if statement around all of the loop so as to enable and disable the loop
-        if (Enable == 1) begin
+              S_010: begin
+		 // A & B
+		 Data_A = X;
+		 Data_B = Y;
+		 
+		 Results = Data_A & Data_B;
+		 
+              end // S_010
+	      
+              S_011: begin
+		 // A | B
+		 Data_A = X;
+		 Data_B = Y;
+		 
+		 Results = Data_A | Data_B;
+		 
+              end // S_011
 
-        case (State)
+              S_100: begin
+		 // A < B
+		 // Results <= Data_A < Data_B;
+		 Data_A = X;
+		 Data_B = Y;
+		 
+		 if (Data_A < Data_B) begin
+		    CF = 1;
+		 end else begin 
+		    CF = 0;
+		 end
 
-            S_000: begin
-                // NOP Operation
-                StateNext <= S_000;
-                #10; 
-                // Simply Does Nothing
-            end // S_000
-            
-            S_001: begin
-                // A + B
-                Results = Data_A + Data_B;
-                StateNext <= S_000;
-                #10;
-                // How to notice overflow? 
-            end // S_001
+		 Results = CF;
+		 		 
+              end // S_100
 
-            S_010: begin
-                // A & B
-                Results = Data_A & Data_B;
-                StateNext <= S_000;
-                // Should just be this
-            end // S_010
+              S_101: begin
+		 // A + B (write results to A)
+		 Data_B = Y;
+		 Results = Data_A + Data_B;
+		 Data_A = Results;
+		 
+		 OverflowCheck = Results;
+		 
+		 // check for overflow
+		 if (OverflowCheck[16] == 1) begin
+		    CF = 1;
+		 end else begin
+		    CF = 0;
+		 end
+		 
+              end // S_101
 
-            S_011: begin
-                // A | B
-                Results = Data_A | Data_B;
-                StateNext <= S_000;
-                // Should just be this
-            end // S_011
+              S_110: begin 
+		 // A & B (write results to A)
+		 Data_A = X;
+		 Data_B = Y;
+		 
+		 Data_A = Data_A & Data_B;
+		 Results = Data_A;
+		 		 
+              end // S_110
 
-            S_100: begin
-                // A < B
-                // Results <= Data_A < Data_B;
-                StateNext <= S_000;
-            end // S_100
+              S_111: begin
+		 // A | B (write results to A)
+		 Data_A = X;
+		 Data_B = Y;
+		 
+		 Data_A = Data_A | Data_B;
+		 Results = Data_A;
+		 
+              end // S_111
 
-            S_101: begin
-                // A + B (write results to A)
-                Data_A <= Data_A + Data_B;
-                StateNext <= S_000;
-                // How to set overflow flag?
-            end // S_101
-
-            S_110: begin
-                // A & B (write results to A)
-                Data_A <= Data_A & Data_B;
-                StateNext <= S_000;
-                // Should just be this
-            end // S_110
-
-            S_111: begin
-                // A | B (write results to A)
-                Data_A <= Data_A | Data_B;
-                StateNext <= S_000;
-                // Should just be this
-            end // S_111
-
-        endcase // case (State)
-
-      end // if loop
-
-   end // always @ (State)
-
-
-    // At Reset = 1, all internal registers should be in a known state or set to 0. 
-    always @(posedge CLK) begin
-        if (RST == 1) begin
-
-            // Initialize Values if not known
-            if (Data_A < 0) begin
-                Data_A <= 0;
-            end 
-            if (Data_B < 0) begin
-                Data_B <= 0;
-            end
-
-            // Reset everything to Zero
-            Results <= 0;
-            Opcode <= 0;
-	        State <= S_000;
-            CF = 0;
-
-        end else begin
-	        State <= StateNext;
-        end
-    end // always end
-
-// Bunch of condiional statements depending on what the opcode is
-// If the opcode starts with a 0, then take input
-// If the opcode starts with a 1, then look for input
-// Look at making state names
-
+            endcase // case (State)
+	    
+	 end   
+      end
+   end // always end
+      
+   // Bunch of condiional statements depending on what the opcode is
+   // If the opcode starts with a 0, then take input
+   // If the opcode starts with a 1, then look for input
+   // Look at making state names
+      
 endmodule
--- Sam/ALU_testbench.v
+++ Sam/ALU_testbench.v
@@ -6,105 +6,225 @@
 
 module ALU_testbench();
 
-    reg CLK_s, RST_s, Enable_s;
-    reg [15:0] Data_A_s;
-    reg [15:0] Data_B_s; 
-    reg [3:0] Opcode_s;
+   reg CLK_s, RST_s, Enable_s;
+   reg [15:0] Data_A_s;
+   reg [15:0] Data_B_s; 
+   reg [2:0]  Opcode_s;
+   
+   // Output defintions here
+   wire	      CF_s;                   
+   wire [15:0] Results_s;      
+   
+   ALU ALU_Test_Functions(CLK_s, RST_s, Enable_s, Data_A_s, Data_B_s, Opcode_s, CF_s, Results_s);
+   
+   // Basic Clock
+   always begin
+      CLK_s <= 0;
+      #10;
+      CLK_s <= 1;
+      #10;
+   end
 
-    // Output defintions here
-    reg CF;                   
-    output wire [15:0] Results;      
+   initial begin
+      $dumpvars(0, ALU_testbench); // Include name of tb file
+      // init input data
+      Data_A_s <= 'h0000;
+      Data_B_s <= 'h0000;
+      Enable_s <= 0;
+      Opcode_s <= 0;
+      RST_s <= 0;
+      
+      
+      #20;
+      
+      @(posedge CLK_s)
+        RST_s <= 1;
+      
+      @(posedge CLK_s)
+        RST_s <= 0;
+      
+      #100;
+      
+      // Enable is off, Data is present, try to Add
+      
+      @(posedge CLK_s) begin
+         Enable_s <= 0;
+	 Data_A_s <= 'h6464;
+	 Data_B_s <= 'h4646;
+	 Opcode_s <= 'b001;
+      end
+      
+      #100;
+      
+      // Add Data_A and Data_B
+      @(posedge CLK_s) begin
+        Enable_s <= 1;
+      end
+      
+      #100;
+      
+      // Add Data_A and Data_B, then try to & them, but have the enable off.
+      @(posedge CLK_s) begin
+         Enable_s <= 0;
+      end
 
-    reg [2:0] test_case = 1;
+      @(posedge CLK_s) begin
+	 Opcode_s <= 'b010;
+      end
+	 
+      // Enable high
+      @(posedge CLK_s) begin
+	 Enable_s <= 1;
+      end
 
-    ALU ALU_Test_Functions(CLK_s, RST_s, Enable_s);
+      #100;
+      
+      // Enable Low
+      @(posedge CLK_s) begin
+         Enable_s <= 0;
+      end
+      
+      // A OR B
+      @(posedge CLK_s) begin
+	 Opcode_s <= 'b011;
+      end
+      
+      // Enable high
+      @(posedge CLK_s) begin
+	 Enable_s <= 1;
+      end
 
-    // Basic Clock
-    always begin
-        CLK_s <= 0;
-        #10;
-        CLK_s <= 1;
-        #10;
-    end
+      // Enable Low
+      @(posedge CLK_s) begin
+         Enable_s <= 0;
+      end
 
-    initial begin
-        $dumpvars(0, ALU_testbench); // Include name of tb file
+      // A LESS THAN B
+      @(posedge CLK_s) begin
+	 Opcode_s <= 'b000;
+      end
+      
+      // Enable high
+      @(posedge CLK_s) begin
+	 Enable_s <= 1;
+      end
+ 
+      // Enable low
+      @(posedge CLK_s) begin
+	 Enable_s <= 0;
+      end
 
-        case (test_case)
-            
-            1: begin // Reset The System
+      // A LESS THAN B
+      @(posedge CLK_s) begin
+	 Opcode_s <= 'b100;
+      end
+      
+        // Enable high
+      @(posedge CLK_s) begin
+	 Enable_s <= 1;
+      end
+ 
+      // Enable low
+      @(posedge CLK_s) begin
+	 Enable_s <= 0;
+      end
 
-                #20
-                @(posedge CLK_s)
-                RST_s <= 1;
-                #20;
+      // A AND B
+      @(posedge CLK_s) begin
+	 Opcode_s <= 'b101;
+      end
+      
+      // Enable high
+      @(posedge CLK_s) begin
+	 Enable_s <= 1;
+      end
+ 
+      // Enable low
+      @(posedge CLK_s) begin
+	 Enable_s <= 0;
+      end
 
-                @(posedge CLK_s)
-                RST_s <= 0;
-                #200;
+      // A OR B
+      @(posedge CLK_s) begin
+	 Opcode_s <= 'b101;
+      end
 
-            end // End Test Case 1
+      // Enable high
+      @(posedge CLK_s) begin
+	 Enable_s <= 1;
+      end
+ 
+      // Enable low
+      @(posedge CLK_s) begin
+	 Enable_s <= 0;
+      end
 
-            2: begin // Enable is off, Data is present, try to Add
+      // A OR B
+      @(posedge CLK_s) begin
+	 Opcode_s <= 'b101;
+      end
 
-                #20
-                @(posedge CLK_s)
-                Enable_s <= 0;
-                Data_A_s <= 6464;
-                Data_B_s <= 4646;
-                Opcode_s <= 001;
-                #200;
+      // Enable high
+      @(posedge CLK_s) begin
+	 Enable_s <= 1;
+      end
+ 
+      // Enable low
+      @(posedge CLK_s) begin
+	 Enable_s <= 0;
+      end
 
-                // Nothing should happen
+      // A OR B
+      @(posedge CLK_s) begin
+	 Opcode_s <= 'b000;
+      end
+      
+      // Enable high
+      @(posedge CLK_s) begin
+	 Enable_s <= 1;
+      end
+ 
+      // Enable low
+      @(posedge CLK_s) begin
+	 Enable_s <= 0;
+      end
 
-            end
+      // A OR B
+      @(posedge CLK_s) begin
+	 Opcode_s <= 'b110;
+      end
 
-            3: begin // Add Data_A and Data_B
+      // Enable high
+      @(posedge CLK_s) begin
+	 Enable_s <= 1;
+      end
+ 
+      // Enable low
+      @(posedge CLK_s) begin
+	 Enable_s <= 0;
+      end
 
-                #20
-                @(posedge CLK_s)
-                Enable_s <= 0;
-                Data_A_s <= 6464;
-                Data_B_s <= 4646;
-                Opcode_s <= 001;
-                #20;
+      // A OR B
+      @(posedge CLK_s) begin
+	 Opcode_s <= 'b111;
+      end
 
-                @(posedge CLK_s)
-                Enable_s <= 1;
-                #200;
+      // Enable high
+      @(posedge CLK_s) begin
+	 Enable_s <= 1;
+      end
+ 
+      // Enable low
+      @(posedge CLK_s) begin
+	 Enable_s <= 0;
+      end
 
-                // Now things should happen
 
-            end
-
-            4: begin // Add Data_A and Data_B, then try to & them, but have the enable off.
-
-                #20
-                @(posedge CLK_s)
-                Enable_s <= 0;
-                Data_A_s <= 6464;
-                Data_B_s <= 4646;
-                Opcode_s <= 001;
-                #20;
-
-                @(posedge CLK_s)
-                Enable_s <= 1;
-                #20;
-
-                @(posedge CLK_s)
-                Enable_s <= 0;
-                Opcode_s <= 010;
-                #200;
-
-                // Nothing should happen after the add
-
-            end
-
-        endcase // End Test Case Selection
-
-    @(posedge CLK_s)
+      #200;
+      
+      @(posedge CLK_s)
         $finish;
-
-    end // Initial Begin
-
+      
+   end // Initial Begin
+   
 endmodule // End of ALU Testbench
