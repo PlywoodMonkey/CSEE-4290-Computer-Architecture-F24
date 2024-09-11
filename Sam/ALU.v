53b776ad3a64a6370716b7063adb4ab92c9e4cc2
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2024-09-11 14:27:15 -0400

    Update Before Test Run

--- Sam/ALU.v
+++ Sam/ALU.v
@@ -9,19 +9,19 @@ module ALU (CLK, RST, Enable);
     // Input definitions here
     input CLK, RST, Enable;
     reg [15:0] Data_A;
-    reg [15:0] Data_B; 
+    reg [15:0] Data_B;
     reg [3:0] Opcode;
 
     // Output defintions here
     output reg CF;                  // overflow bit 
-    output reg [15:0] Results;  
+    output reg [15:0] Results;
 
     // Define States for ALU
     parameter  S_000 = 000, S_001 = 001, S_010 = 010, S_011 = 011, S_100 = 100, S_101 = 101, S_110 = 110, S_111 = 111;
 
     // Variables for Sequence Logic
     reg [3:0] State, StateNext;
-/*
+
     always @(State) begin
 
         // put this if statement around all of the loop so as to enable and disable the loop
@@ -90,16 +90,26 @@ module ALU (CLK, RST, Enable);
       end // if loop
 
    end // always @ (State)
-*/
+
 
     // At Reset = 1, all internal registers should be in a known state or set to 0. 
     always @(posedge CLK) begin
         if (RST == 1) begin
-            Data_A <= 0;
-            Data_B <= 0;
+
+            // Initialize Values if not known
+            if (Data_A < 0) begin
+                Data_A <= 0;
+            end 
+            if (Data_B < 0) begin
+                Data_B <= 0;
+            end
+
+            // Reset everything to Zero
             Results <= 0;
             Opcode <= 0;
 	        State <= S_000;
+            CF = 0;
+
         end else begin
 	        State <= StateNext;
         end
